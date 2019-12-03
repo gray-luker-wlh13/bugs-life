0de69878a4a2fa4a5712d4d6b49e90a581fc5b2b
grayluker05 <graysonluker@gmail.com>
2019-12-03 09:19:20 -0700

    changed spelling and props

--- bugs-2/src/App.js
+++ bugs-2/src/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
     };
   }
 
-  renber() {
+  render() {
     const items = this.state.groceryList.map((item, index) => {
       return <Item key={index} item={item} />;
     });
